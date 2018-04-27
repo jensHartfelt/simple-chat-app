b97c24229e21f0effb404e3e1b37d4a18087f510
Jens Hartfelt <jenshartfelt@Jenss-MacBook-Pro-2.local>
2018-04-27 12:37:34 +0200

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,234 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    // The socket for getting and posting messages
+    socket: undefined,
+    // The current users display name
+    displayName: "",
+    // Uniquely generted id to identify the author
+    userId: "",
+    // What user has types in the textarea
+    clientMessage: "",
+    // Array of current messages
+    messages: [],
+    // Boolean that determines if the overlay should be visible
+    overlayActive: true,
+    // Amount of users currently connected to the chat
+    usersOnline: 0,
+    // Amount of users with unsent changes in their text-field
+    usersTyping: 0,
+    // Weather the app is ready or not
+    appReady: false,
+    // browser -> "chrome, safari, firefox or other"
+    browser: "",
+    // Reference to the body
+    bodyEl: document.querySelector("body")
+  },
+  methods: {
+    verifyDisplayName: function() {
+      if (this.displayName) {
+        this.overlayActive = false;
+        localStorage.setItem("displayName", this.displayName);
+      }
+    },
+    emojifyText(text) {
+      text = text.replace(/:-\)|:\)|:D|:-D/g, "😊");
+      text = text.replace(/:-\(|:\(/g, "😢");
+      return text;
+    },
+    sendMessage: function() {
+      /**
+       * Sends clients message to the server
+       */
+
+      // If the message if empty, don't send it
+      if (this.clientMessage === "") {
+        return;
+      }
+
+      // Create the message
+      var message = {
+        time: Date.now(),
+        author: this.displayName,
+        authorId: this.userId,
+        content: this.emojifyText(this.clientMessage)
+      };
+
+      // Clear current client message
+      this.clientMessage = "";
+
+      // Send message to all other users than the client
+      this.socket.emit("new message from client", message);
+
+      // Locally add the message to the clients array of messages
+      // -> Making it appear instantely available
+      this.pushToMessagesArray(message);
+    },
+    pushToMessagesArray: function(message) {
+      this.messages.push(message);
+      this.scrollNewMessageIntoView();
+    },
+    scrollNewMessageIntoView: function() {
+      /**
+       * Scrolls messages into view whenever a new message is added.
+       */
+      var context = this; // If the project had a build step i would've used arrow-functions instead
+
+      // Event loop hack. Wait's for dom to be updated before executing
+      Vue.nextTick(function() {
+        var totalScrollHeight = context.bodyEl.scrollHeight;
+        var curScroll = context.bodyEl.scrollTop;
+        var screenHeight = window.innerHeight;
+        var maxScroll = totalScrollHeight - screenHeight;
+        // Arguments list: from value, to value, duration, callback (that applies the animation), easing function
+        TinyAnimate.animate(
+          curScroll,
+          maxScroll,
+          120,
+          function(newScroll) {
+            context.bodyEl.scrollTop = newScroll;
+          },
+          "ease"
+        );
+      });
+    },
+    deleteSavedDisplayName: function() {
+      /**
+       * Deletes the save displayName from lovalstorage and retrigger
+       * the modal where you have to choose a display name
+       */
+      localStorage.removeItem("displayName");
+      this.displayName = "";
+      this.overlayActive = true;
+    },
+    updateConnectedUsers: function(connectedUsers) {
+      /**
+       * Shows how many users are connected. The curent client is included
+       * in the number of connected users, so the number is substracted by one
+       */
+      if (connectedUsers - 1 < 1) {
+        this.usersOnline = 0;
+      } else {
+        this.usersOnline = connectedUsers - 1;
+      }
+    },
+    setExistingMessages(messages) {
+      /**
+       * Updates the list of messages with the one that the server already has
+       */
+      this.messages = messages;
+      this.appReady = true;
+      this.scrollNewMessageIntoView();
+      this.runWelcomeSequence();
+    },
+    determineBrowserType() {
+      var userAgent = navigator.userAgent.toLowerCase();
+      if (userAgent.indexOf("chrome") !== -1) {
+        this.browser = "chrome";
+      } else if (userAgent.indexOf("firefox") !== -1) {
+        this.browser = "firefox";
+      } else if (userAgent.indexOf("safari") !== -1) {
+        this.browser = "safari";
+      } else {
+        this.browser = "other";
+      }
+    },
+    saveUniqueId(uniqueId) {
+      localStorage.setItem("userId", uniqueId);
+      this.uniqueId = uniqueId;
+    },
+    runWelcomeSequence() {
+      var welcomeMessages = [
+        "Send messages by typing your message and pressing enter",
+        "You kan make line breaks by holding down the shift key",
+        "The following shorthands are converted to emojis: \n:) \n:-) \n:D \n:( \n:-(",
+        "The chat app is globally available to everyone but stores only the last 500 messages",
+        "Enjoy 😊"
+      ];
+
+      this.messages.push({
+        time: Date.now(),
+        author: "Simple Chat App",
+        content: "Hi! Welcome to the chat app"
+      });
+
+      var iterator = 0;
+      var interval = setInterval(
+        function() {
+          this.messages.push({
+            time: Date.now(),
+            author: "Simple Chat App",
+            content: welcomeMessages[iterator]
+          });
+          iterator++;
+          if (iterator >= 5) {
+            clearInterval(interval);
+          }
+        }.bind(this),
+        3000
+      );
+    }
+  },
+  created() {
+    /**
+     * Lifecycle method ->
+     * This function runs as soon as the main vue-app is ready.
+     * It connects to the socket and save the connection to the
+     * vue-instance. There is a bunch of useful lifecycle methods
+     * ie. beforeUpdate or afterUpdate that runs every time the
+     * component renders
+     */
+    this.socket = io.connect("http://localhost:8000");
+
+    // Get user id
+    if (!localStorage.getItem("userId")) {
+      this.socket.on("unique id generated", this.saveUniqueId);
+    } else {
+      this.userId = localStorage.getItem("userId");
+    }
+
+    // Listen for new messages
+    this.socket.on("new message", this.pushToMessagesArray);
+
+    // Listen for connected users updates:
+    this.socket.on("connected users update", this.updateConnectedUsers);
+
+    // try to load display name from local storage -> bypasses modal if it succeeds
+    var savedDisplayName = localStorage.getItem("displayName");
+    if (savedDisplayName) {
+      this.displayName = savedDisplayName;
+      this.overlayActive = false;
+    }
+
+    // Get existing messages
+    var request = new XMLHttpRequest();
+    request.onload = function(res) {
+      this.setExistingMessages(JSON.parse(res.target.response));
+    }.bind(this);
+    request.open("GET", "/messages");
+    request.send();
+
+    // Determine browser type
+    this.determineBrowserType();
+  },
+  watch: {
+    /**
+     * Watcher function is triggers a function whenever a value change. You can watch any value
+     * within the app.data object.
+     * The name of the function (ie. XXXXXXX: function(val) {}) has to match the name of the
+     * values being watched
+     */
+    clientMessage: function(val) {
+      if (val && val.length === 1) {
+        this.socket.emit("someone is typing");
+      } else if (!val) {
+        this.socket.emit("someone stopped typing");
+      }
+    }
+  }
+});
+
+// Shows the app -> Super simple hack to prevent malformed html when loading
+document.getElementById("app").style.display = "block";
+var appLoader = document.getElementById("mainAppLoader");
+appLoader.parentElement.removeChild(appLoader);
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io").listen(8000);
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+/********* ROUTES *********/
+// I delibaretely read this file syncronously. This will only happen before
+// server is live and accepting trafic. It's therefor actually wanted for
+// node to wait for this file until going online
+var messages = JSON.parse(fs.readFileSync(__dirname + "/data.json", "utf8"));
+
+// Public files
+app.use(express.static("public"));
+
+// Get existing messages
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+/********* SOCKETS *********/
+
+// Holds information about how many users are connected right now
+var connectedUsers = 0;
+
+// Event that fires when a user connects
+io.on("connection", socket => {
+  // Increase the amount of connected users and tell all other users
+  // about the change
+  connectedUsers++;
+  io.emit("connected users update", connectedUsers);
+
+  socket.emit("unique id generated", uniqid());
+
+  // Broadcasts messages whenever one is recieved
+  socket.on("new message from client", msg => {
+    socket.broadcast.emit("new message", msg);
+
+    // Save messages in local variable
+    if (messages.length >= 50) {
+      messages.shift();
+    }
+    messages.push(msg);
+
+    // Save messages to file
+    storeMessages();
+  });
+
+  // When a user disconnects, tell other users about the new
+  // amount of connected users
+  socket.on("disconnect", () => {
+    connectedUsers--;
+    io.emit("connected users update", connectedUsers);
+  });
+});
+
+/********* SERVER *********/
+
+http.listen(80, () => {
+  console.log("listening on port 80");
+});
+
+/******** UTILITIES *********/
+var timeout;
+function storeMessages() {
+  /**
+   * Debounced message-saving. Will only save to file after 1s of inactivity
+   *
+   */
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    var stringifiedMessages = JSON.stringify(messages);
+    fs.writeFile(__dirname + "/data.json", stringifiedMessages, (err, res) => {
+      console.log(
+        "Messages saved! File now contains " + messages.length + " messages"
+      );
+    });
+  }, 1000);
+}
